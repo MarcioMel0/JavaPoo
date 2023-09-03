f35f19d2f01d6d71ca3b3014f0608c6cbf20f064
Marcio <49918509+MarcioMel0@users.noreply.github.com>
2023-09-03 20:55:37 -0300

    Add files via upload

---
+++ Revisao01/src/main/java/com/mycompany/revisao01/Main.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.revisao01;
+
+/**
+ *
+ * @author Marcio Melo
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        Caneta c1 = new Caneta("Bic", "Vermelho", 0.9f);
+        c1.status();
+      
+    }
+}
